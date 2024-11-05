f6c9bd9e8489a5388f39b5d8ee3fb6cddaaf6d19
Shabbeer Petersen <shabbeerpetersen10@gmail.com>
2024-11-05 14:49:56 +0200

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config();
+dotenv.config({path:'./config.env'});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -18,5 +17,5 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
